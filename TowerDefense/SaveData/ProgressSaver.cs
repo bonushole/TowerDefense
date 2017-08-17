47b1a965ea4be5a9e1f7846a013ae2743a393f45
Maximilian Evans <156max@gmail.com>
2017-08-17 17:49:49 -0400

    added files for a json progress saving solution

---
+++ TowerDefense/SaveData/ProgressSaver.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TowerDefense.SaveData
+{
+    class ProgressSaver
+    {
+        public bool existingSave;
+
+        public int gold;
+        public int level;
+
+        public void ReadSave()
+        {
+
+            StreamReader reader = new StreamReader("Progress.json");
+
+
+        }
+        public void WriteSave() {
+
+
+
+        }
+
+    }
+}
